e55d65225d98f868a3ec4b065cbaa522c6bc917f
jerinjoseputhussery <60233550+jerinjoseputhussery@users.noreply.github.com>
2024-04-04 22:36:17 +0100

    Script and code improvements
    SQL Scripts for dynamic tables added
    Fetching current routes added

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import requests
+import json
+import pyodbc
 from config import api_url,subscription_key
+from static_loader_config import database,password,server,username
+
+def getStaticRoutes():
+    conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
+    cursor = conn.cursor()
+    cursor.execute('SELECT route_id,agency_id,route_short_name,route_long_name FROM [dublin_bus].[dbo].[routes] where agency_id in (select agency_id from agency where agency_name like \'%Dublin Bus%\')')
+    rows = cursor.fetchall()
+    return rows
+
 def call_api_and_display_response(api_url, headers):
     try:
         # Make a GET request to the API
@@ -9,8 +20,22 @@ def call_api_and_display_response(api_url, headers):
         if response.status_code == 200:
             # Display the API response content
             print("API Response:")
+            jsonResponse=json.loads(response.text)
             # print(response.text)
-            
+            print('Entity count: ',len(jsonResponse["entity"]))
+            count=0
+            total_arrival_delays=0            
+            routesFromDatabase = [row[0] for row in getStaticRoutes()]
+            currentRoutes=set()
+            for entities in jsonResponse["entity"]:
+                if(entities["trip_update"]["trip"]["schedule_relationship"]=='SCHEDULED'):                    
+                    count+=1
+                    if(entities["trip_update"]["trip"]["route_id"] in routesFromDatabase):
+                        currentRoutes.add(entities["trip_update"]["trip"]["route_id"])
+                    # for stop_time_update in entities["trip_update"]["stop_time_update"]:
+                    #     stop_time_update["arrival"]["delay"]
+            print('SCHEDULED: ',count)
+            print('Current',len(currentRoutes),'DUBLIN BUS Routes: ',currentRoutes)
         else:
             # Display an error message for unsuccessful requests
             print(f"Error: {response.status_code} - {response.text}")
--- create_GTFS_static.sql
+++ create_GTFS_static.sql
@@ -18,3 +18,49 @@ ALTER TABLE stop_times ADD FOREIGN KEY (trip_id) REFERENCES trips(trip_id);
 ALTER TABLE stop_times ADD FOREIGN KEY (stop_id) REFERENCES stops(stop_id);
 
 
+--DYNAMIC TABLES
+Create table route_delays (entry_id int, route_id varchar(20), direction_id varchar(10), entry_timestamp timestamp, current_delay int, avg_route_delay int,
+PRIMARY KEY (entry_id,route_id,direction_id,entry_timestamp));
+Create table delays(entry_id int, entry_timestamp timestamp, current_delay int, avg_delay int, primary key(entry_id,entry_timestamp));
+Create table weather (entry_id int, location varchar(200), entry_timestamp timestamp,temperature_2m	FLOAT,relative_humidity_2m int,apparent_temperature	FLOAT,is_day int,precipitation int,rain int,showers int, snowfall int, weather_code int, cloud_cover int, pressure_msl FLOAT, surface_pressure FLOAT, wind_speed_10m FLOAT,wind_direction_10m int, wind_gusts_10m FLOAT, avg_temperature_2m	FLOAT, avg_relative_humidity_2m int, avg_apparent_temperature	FLOAT, avg_precipitation int, avg_rain int, avg_showers int, avg_snowfall int, avg_cloud_cover int, avg_pressure_msl FLOAT, avg_surface_pressure FLOAT, avg_wind_speed_10m FLOAT, avg_wind_direction_10m int, avg_wind_gusts_10m FLOAT, primary key(entry_id,entry_timestamp));
+create table weather_codes( code int primary key,code_description varchar(100), code_count int, avg_delay int);
+
+--INSERT QUERIES TO weather_codes table
+insert into weather_codes values(0,'Clear sky',0,0);
+insert into weather_codes values(1,'Mainly clear',0,0);
+insert into weather_codes values(2,'partly cloudy',0,0);
+insert into weather_codes values(3,'overcast',0,0);
+insert into weather_codes values(45,'Fog',0,0);
+insert into weather_codes values(48,'depositing rime fog',0,0);
+insert into weather_codes values(51,'Light Drizzle',0,0);
+insert into weather_codes values(53,'Moderate Drizzle',0,0);
+insert into weather_codes values(55,'Dense Drizzle',0,0);
+insert into weather_codes values(56,'Light Freezing Drizzle',0,0);
+insert into weather_codes values(57,'Dense Freezing Drizzle',0,0);
+insert into weather_codes values(61,'Slight Rain',0,0);
+insert into weather_codes values(63,'Moderate Rain',0,0);
+insert into weather_codes values(65,'Heavy Rain',0,0);
+insert into weather_codes values(66,'Light Freezing Rain',0,0);
+insert into weather_codes values(67,'heavy Freezing Rain',0,0);
+insert into weather_codes values(71,'Slight Snow fall',0,0);
+insert into weather_codes values(73,'moderate Snow fall',0,0);
+insert into weather_codes values(75,'Heavy Snow fall',0,0);
+insert into weather_codes values(77,'Snow grains',0,0);
+insert into weather_codes values(80,'Slight Rain showers',0,0);
+insert into weather_codes values(81,'Moderate Rain showers',0,0);
+insert into weather_codes values(82,'Violent Rain showers',0,0);
+insert into weather_codes values(85,'Slight Snow showers',0,0);
+insert into weather_codes values(86,'Heavy Snow showers',0,0);
+insert into weather_codes values(95,'Slight or moderate Thunderstorm',0,0);
+insert into weather_codes values(96,'Thunderstorm with slight hail',0,0);
+insert into weather_codes values(99,'Thunderstorm with heavy hail',0,0);
+
+--TRIGGERS
+CREATE TRIGGER weather_code_trigger
+ON weather
+AFTER INSERT
+AS
+BEGIN    
+    UPDATE weather_codes SET code_count = code_count + 1 WHERE code=(SELECT weather_code FROM inserted);	
+END;
+
