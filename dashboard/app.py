399ca88361709289cc33b11f1b85d5f1f86c3ab2
jerinjoseputhussery <60233550+jerinjoseputhussery@users.noreply.github.com>
2024-05-07 00:30:57 +0100

    Pie chart added
    -Pie chart added
    -Started with few styling

--- dashboard/app.py
+++ dashboard/app.py
@@ -8,7 +8,19 @@ import plotly.graph_objects as go
 from charts import gauge_chart,bar_chart,line_chart,day_in_week_chart
 from methods import giveMeRoutes,giveMeRouteName,giveMeDFs,giveMe4DFs
 
-app = Dash(__name__)
+
+external_stylesheets = [
+    {
+        "href": (
+            "https://fonts.googleapis.com/css2?"
+            "family=Lato:wght@400;700&display=swap"
+        ),
+        "rel": "stylesheet",
+    },
+]
+app = Dash(__name__,external_stylesheets=external_stylesheets)
+app.title = "Dublin Bus Delay Dashboard"
+
 routes_dict = giveMeRoutes()
 routes = list(routes_dict.keys())
 routes.insert(0, 'All-routes')
@@ -16,8 +28,9 @@ routes.insert(0, 'All-routes')
 
 # Define the layout of the app
 app.layout = html.Div(children=[
-    dcc.Interval(id='refresh', interval=60000),
-    html.H1(children='Dublin Bus Delay Dashboard'),   
+    # dcc.Interval(id='refresh', interval=60000),
+    html.Div(children=[
+    html.H1(children='Dublin Bus Delay Dashboard',className="header-title"),],className="header"),   
     dcc.Dropdown(routes, 'All-routes', id='route-selection',style={'width':'35%','display':'inline-block'}), 
     dcc.Dropdown(id='direction-selection',style={'width':'35%','display':'inline-block'},disabled=True), 
     dcc.Dropdown(['Today','All-time'], 'Today', id='dropdown-selection',
@@ -33,13 +46,14 @@ app.layout = html.Div(children=[
     ),
     dcc.Graph(id='bar-chart1'),
     dcc.Graph(id='bar-chart2'),
+    dcc.Graph(id='pie-chart1'),
     html.Div(id='last_update_time',
              style={'font-size': '10px'}),
     dcc.Interval(
             id='interval-component',
             interval=5*60*1000, # in milliseconds
             n_intervals=0
-    )
+    )    
 
 ])
 @app.callback(
@@ -57,12 +71,13 @@ def set_direction(route_short_name):
     routes_dir.insert(0, 'Both-directions')
     return routes_dir,disabled,'Both-directions'
 
-@app.callback(             
+@app.callback(       
               Output('graph1', 'figure'),
               Output('graph2', 'figure'), 
               Output('timeline', 'figure'),
               Output('bar-chart1', 'figure'),
               Output('bar-chart2', 'figure'),
+               Output('pie-chart1', 'figure'),
               Output('last_update_time', 'children'),             
               [Input('route-selection', 'value'),
                Input('direction-selection', 'value'),
@@ -88,7 +103,6 @@ def update_refresh(route_short_name,direction_title,value,n):
         cursor.execute('select TOP 1 current_delay from delays where entry_id < (SELECT MAX(entry_id) FROM delays) order by entry_id desc')
         prev_delay = cursor.fetchone()[0]
     else:
-        print('direction_title',direction_title)
         if(direction_title=='Both-directions' or direction_title==None):
             cursor.execute('SELECT AVG(current_delay) FROM route_delays where route_id=?',routes_dict[route_short_name])
             avg_delay =  cursor.fetchone()[0]
@@ -134,9 +148,17 @@ def update_refresh(route_short_name,direction_title,value,n):
     colors = ['lightslategray',] * 7
     colors[df['y'].idxmax()] = 'crimson'  
     bar2=day_in_week_chart('Delay with Day of Weeks',df,'x','y','Day of week','Average Delay',colors)
+    
+    
+    #Pie chart
+    cursor.execute('select code_description,code_count from weather_codes where code_count>0')    
+    pie1 = px.pie(giveMeDFs(cursor.fetchall()), values='y', names='x') 
+    pie1.update_layout(
+        title='Dublin Weather'
+    )   
     print(datetime.datetime.now())
     cursor.close()
     conn.close()
-    return fig1,fig2,fig0,bar1,bar2,html.P('Last updated ' +str(datetime.datetime.now()))
+    return fig1,fig2,fig0,bar1,bar2,pie1,html.P('Last updated ' +str(datetime.datetime.now()))
 if __name__ == '__main__':
     app.run(debug=True)
